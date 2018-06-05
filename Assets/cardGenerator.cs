1ddd543aa111b4c72dcee17c460c9cc88a2e3b86
Timur <tima96@bk.ru>
2018-06-05 18:05:36 +0300

    Создал основу
    Создал основу

---
+++ Assets/cardGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cardGenerator : MonoBehaviour {
+    public List<GameObject> cards = new List<GameObject>();
+    // Use this for initialization
+    void Start () {
+	}
+	
+    public void getNewCard()
+    {
+        int nextCard = Random.Range(0, cards.Count - 1);
+        cards[nextCard].SetActive(true);
+    }
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
---
+++ Assets/showTheFutureChanges.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showTheFutureChanges : MonoBehaviour {
+    public GameObject familyFutureCircle;
+    public GameObject friendFutureCircle;
+    public GameObject girlFutureCircle;
+    public GameObject classFutureCircle;
+    // Use this for initialization
+    void Start () {
+        familyFutureCircle.transform.localScale += new Vector3(2F, 2F, 2F);
+    }
+
+    public void familyFuture()
+    {
+        familyFutureCircle.transform.localScale += new Vector3(2F, 2F, 2F);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
