0a551e1a91eeaba83b2cad42e9b576f438e2e7af
Timur <tima96@bk.ru>
2018-06-09 13:59:33 +0300

    Добавил ассеты новеллы

---
+++ Assets/VisualNovelToolkit/defender.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class defender : MonoBehaviour {
+
+    public GameObject guiTextLink; //Ссылка на объект, если скрипт не на нем
+    public TextButton tb;
+    public void changeColor()
+    {
+        guiTextLink.GetComponent<Text>().color = new Color(0, 0, 1, 1);
+        Debug.Log("1111");
+    }
+}
