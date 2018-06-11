2d36f5b3b68bac57ca982652f12cb0ea0b8d0136
sfalexrog <git@github.com>
2018-06-11 19:42:52 +0300

    Add a "hub" scene

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    private static object _lock = new object();
+
+    private static T Instance
+    {
+        get
+        {
+            if (_applicationIsQuitting)
+            {
+                Debug.LogWarning("Attempted to access singleton on application quit");
+                return null;
+            }
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = (T) FindObjectOfType(typeof(T));
+                    if (FindObjectsOfType(typeof(T)).Length > 1)
+                    {
+                        Debug.LogError("Multiple instances of a singleton");
+                        return _instance;
+                    }
+
+                    if (_instance == null)
+                    {
+                        var singleton = new GameObject();
+                        _instance = singleton.AddComponent<T>();
+                        singleton.name = "(singleton) " + typeof(T).ToString();
+                        
+                        DontDestroyOnLoad(singleton);
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+
+    private static bool _applicationIsQuitting = false;
+
+    public void OnDestroy()
+    {
+        _applicationIsQuitting = true;
+    }
+}
---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour {
+
+	public void LoadNovelScene(int sceneNum)
+	{
+		SceneManager.LoadScene(sceneNum);
+	}
+	
+	
+}
